2fdb107034712c28f5736516ce17cdde48cca7c4
frecapG7 <florian.recape@wanadoo.fr>
2026-02-13 10:18:20 +0100

    Polls api part1 (#23)

---
+++ routes/polls.mjs
@@ -0,0 +1,72 @@
+import express from "express";
+import { createPoll, deletePoll, getPoll, searchPolls, unvotePoll, votePoll } from "../services/pollsService.mjs";
+import { getTrip } from "../services/tripService.mjs";
+const app = express();
+
+
+app.get("/trips/:tripId/polls", async (req, res) => {
+
+    const {tripId} = req.params;
+    const polls = await searchPolls(tripId, req.query);
+
+
+    const prevCursor = polls.length > 0 ? polls[0]._id : null;
+    const nextCursor = polls.length > 0 ? polls[polls.length - 1]._id : null;
+
+    return res.status(200).json({
+        nextCursor,
+        prevCursor,
+        totalResults: polls.length,
+        polls
+    });
+
+});
+
+app.get("/trips/:tripId/polls/:pollId", async (req , res) => {
+
+    const {tripId, pollId} = req.params;
+    const poll = await getPoll(tripId, pollId);
+    return res.status(200).json(poll);
+});
+
+app.delete("/trips/:tripId/polls/:pollId", async (req , res) => {
+
+    const {tripId, pollId} = req.params;
+    const userId = req.headers["x-user-id"];
+    const poll = await deletePoll(tripId, pollId, userId);
+    return res.status(200).json(poll);
+});
+
+
+app.post("/trips/:tripId/polls", async (req, res) => {
+
+    const {tripId} = req.params;
+    const trip = await getTrip(tripId);
+    const newPoll = await createPoll(trip, req.body);
+    return res.status(201).json(newPoll);
+})
+
+app.patch("/trips/:tripId/polls/:pollId/vote", async (req, res) => {
+
+    const {tripId, pollId} = req.params;
+    const trip = await getTrip(tripId);
+
+    const newPoll = await votePoll(trip, pollId, req.body);    
+    return res.json(newPoll);
+
+})
+
+
+app.delete("/trips/:tripId/polls/:pollId/vote/:optionsId", async (req, res) => {
+
+    const {tripId, pollId, optionsId} = req.params
+    const trip = await getTrip(tripId);
+
+    const userId = req.headers["x-user-id"];
+    
+    const newPoll = await unvotePoll(trip, pollId, optionsId, userId);
+    return res.status(200).json(newPoll);
+
+});
+
+export default app;
---
+++ services/pollsService.mjs
@@ -0,0 +1,192 @@
+import { DatesPoll, HousingPoll, OtherPoll, Poll } from "../models/pollModel.mjs";
+import { InvalidError, NotFoundError } from "../utils/errors.mjs";
+import { verifyDates, verifyUser } from "./validationService.mjs";
+
+
+
+
+export const searchPolls = async (tripId, { limit, cursor, sort = "asc" }) => {
+
+    let query = {
+        trip: tripId
+    };
+
+    if (cursor)
+        query._id = sort === "asc" ? { $gt: cursor } : { $lt: cursor }
+
+    const options = {
+        limit,
+        sort: {
+            createdAt: sort === "asc" ? 1 : -1
+        }
+    }
+
+    const polls = await Poll.find(query, null, options).populate("createdBy voters");
+    return polls;
+}
+
+
+
+
+export const getPoll = async (tripId, pollId) => {
+
+    const poll = await Poll.findOne({
+        trip: tripId,
+        _id: pollId
+    }).populate({
+        path: "options",
+        populate: {
+            path: "selectedBy",
+            model: "TripUser"
+        }
+    }).exec();
+
+
+    if (!poll)
+        throw new NotFoundError("Cannot find poll");
+
+
+    await poll.populate("createdBy");
+    await poll.populate("hasSelected");
+    return poll;
+}
+
+
+export const createPoll = async (trip, poll) => {
+
+    verifyUser(trip, poll.createdBy);
+    let newPoll;
+
+    switch (poll?.type) {
+        case "DatesPoll": {
+            // verify dates are correct
+            poll.options.forEach(({ startDate, endDate }) => verifyDates(startDate, endDate));
+            newPoll = new DatesPoll({
+                ...poll,
+                trip
+            });
+            break;
+        }
+        case "HousingPoll": {
+            //Verify no troll links ? 
+            newPoll = new HousingPoll({
+                ...poll,
+                trip
+            });
+            break;
+        }
+        case "OtherPoll": {
+            newPoll = new OtherPoll({
+                ...poll,
+                trip
+            });
+            break;
+        }
+        default: throw new InvalidError("Invalid poll type");
+    }
+
+
+    const savedPoll = await newPoll.save();
+    await savedPoll.populate("createdBy");
+    return savedPoll;
+}
+
+
+
+export const votePoll = async (trip, pollId, { options, user }) => {
+
+    const poll = await Poll.findOne({
+        trip: trip._id,
+        _id: pollId,
+        isClosed: false
+    });
+
+    if (!poll)
+        throw new NotFoundError("Cannot find poll");
+
+    verifyUser(trip, user);
+
+    if (poll.isSingleAnswer && poll.hasSelected.contains(user))
+        throw new InvalidError("User has already voted on a single answer poll");
+
+
+    options?.forEach(option => {
+        poll.options.filter(o => o._id.equals(option))
+            .filter(o => !o.selectedBy.includes(user._id))
+            .forEach(o => {
+                o.selectedBy.push(user);
+            });
+    })
+
+    const newPoll = await poll.save();
+
+    await newPoll.populate("hasSelected");
+    await newPoll.populate({
+        path: "options",
+        populate: {
+            path: "selectedBy",
+            model: "TripUser"
+        }
+    });
+    return newPoll;
+}
+
+
+
+export const unvotePoll = async (trip, pollId, optionId, userId) => {
+
+    const poll = await Poll.findOne({
+        trip: trip._id,
+        _id: pollId,
+        isClosed: false
+    });
+
+    if (!poll)
+        throw new NotFoundError("Cannot find poll");
+
+    poll.options
+        .filter(o => o._id.equals(optionId))
+        .forEach(o => o.selectedBy = o.selectedBy.filter(user => !user._id.equals(userId)));
+
+    const newPoll = await poll.save();
+    await newPoll.populate("hasSelected");
+    await newPoll.populate({
+        path: "options",
+        populate: {
+            path: "selectedBy",
+            model: "TripUser"
+        }
+    });
+    return newPoll;
+}
+
+
+
+
+export const deletePoll = async (tripId, pollId, userId) => {
+
+    const poll = await Poll.findOne({
+        trip: tripId,
+        _id: pollId,
+        createdBy: userId
+    });
+    if (!poll)
+        throw new NotFoundError("Cannot find poll");
+
+
+    poll.isClosed = true;
+
+    const newPoll = await poll.save();
+    await newPoll.populate("hasSelected");
+    await newPoll.populate({
+        path: "options",
+        populate: {
+            path: "selectedBy",
+            model: "TripUser"
+        }
+    });
+    return newPoll;
+
+
+}
+
