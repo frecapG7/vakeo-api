47a18b9859aea2d60bdb3ab79f8966ca5d03ab17
frecapG7 <florian.recape@wanadoo.fr>
2026-07-01 18:39:25 +0200

    fix(poll): more flexible on single answer and fix poll settings

--- services/pollsService.mjs
+++ services/pollsService.mjs
@@ -104,7 +104,7 @@ export const createPoll = async (trip, poll, user) => {
 
     verifyUser(trip, user);
 
-    const { question, options, type, stop: stopId } = poll;
+    const { question, options, type, stop: stopId, isAnonymous, isSingleAnswer } = poll;
     if (options?.length > POLL_MAX_OPTIONS)
         throw new InvalidError(`Invalid poll options length: max is ${POLL_MAX_OPTIONS}`);
 
@@ -113,7 +113,9 @@ export const createPoll = async (trip, poll, user) => {
         trip: trip._id,
         createdBy: user._id,
         options,
-        ...(stopId && { stop: stopId })
+        ...(stopId && { stop: stopId }),
+        ...(isAnonymous !== undefined && { isAnonymous }),
+        ...(isSingleAnswer !== undefined && { isSingleAnswer })
     };
     const session = await Poll.startSession();
     try {
@@ -232,9 +234,12 @@ export const votePoll = async (trip, pollId, { options, user }) => {
         if (options?.length > 1) {
             throw new InvalidError("Single-answer poll allows only one option");
         }
-        if (userInArray(poll.hasSelected, user)) {
-            throw new InvalidError("User has already voted on this single-answer poll");
-        }
+        // Remove user from all options to replace their vote
+        poll.options.forEach(option => {
+            option.selectedBy = option.selectedBy.filter(
+                u => !userInArray([u], user._id)
+            );
+        });
     }
 
     options?.forEach(optionId => {
